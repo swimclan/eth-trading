4ed38d44d8af0850d9b8b953f75546053c3123ae
Matthew Herron <matthew.herron77@gmail.com>
2019-02-07 19:40:57 -0600

    Fixed utils bug

--- src/utils.js
+++ src/utils.js
@@ -2,6 +2,6 @@ module.exports.filterNullCandles = function(candles) {
   return candles.filter(candle => candle.price && candle.high && candle.low && candle.open);
 }
 
-module.exports.filterLastNCandles(candles, n) {
+module.exports.filterLastNCandles = function(candles, n) {
   return [...candles.slice(-Math.abs(n))];
 }
